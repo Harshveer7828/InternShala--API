4a74ed280f94ae8e82a2e2fe93d298e77f328948
harshveer7828 <harshveer7828@gmail.com>
2024-04-27 13:51:27 +0530

    basic server code is done

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const express = require('express');
 const app = express();
 
 
+//logger
+const logger = require('morgan');
+app.use(logger("tiny"));
+
+app.get('/', (req,res,next)=>{
+    res.status(200).json({messgae:"hello from the / route"})
+})
 
 
+// listening server always at the bottom
 app.listen(process.env.PORT,()=>{
     console.log(`listening on port ${process.env.PORT}`);
 })
