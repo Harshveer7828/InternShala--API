37647d951bd0776d5253f0ba1a2ba26b68f40964
harshveer7828 <harshveer7828@gmail.com>
2024-06-18 22:15:50 +0530

    initilization of jwt

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,6 @@
 const { catchAsyncErrors } = require("../middlewares/catchAsyncErrors.js")
 const Student = require('../models/studentModel.js');
+const { sendtoken } = require("../utils/sendToken.js");
 
 exports.homePage = catchAsyncErrors(async (req, res, next) => {
     res.json({ message: "hello form the route" })
@@ -7,5 +8,6 @@ exports.homePage = catchAsyncErrors(async (req, res, next) => {
 
 exports.studentSignUp = catchAsyncErrors(async (req, res, next) => {
     const student = await new Student(req.body).save();
+    sendtoken(student,201,res);
     res.status(201).json(student);
 });
--- models/studentModel.js
+++ models/studentModel.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
-
+const jwt = require('jsonwebtoken');
 const studentModel = new mongoose.Schema({
 
     email: {
         type: String,
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
         required: [true, "email is required"],
-        unique : true,
-        trim : true
+        unique: true,
+        trim: true
     },
-    password :{
-        type : String,
-        select : false,
-        match:/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
-        maxLength : [20,"Password should not be exceed 20 characters"],
-        minLength : [8,"password should not be less than 8 characters"]
-    
+    password: {
+        type: String,
+        select: false,
+        match: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+        maxLength: [20, "Password should not be exceed 20 characters"],
+        minLength: [8, "password should not be less than 8 characters"]
+
     }
 
 }, { timestamps: true });
 
-const student = mongoose.model('student',studentModel);
+studentModel.methods.getjwttoken = function () {
+    return jwt.sign({ id: this.id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE
+    });
+};
+
+const student = mongoose.model('student', studentModel);
 module.exports = student;
